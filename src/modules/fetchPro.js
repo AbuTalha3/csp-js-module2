adca1300a4b1317c1b970051b06ef8ed19376aed
Siddhartha Ghosh <sghosh@dview.ai>
2023-09-07 23:58:12 +0530

    fixed linters errors

--- src/modules/fetchPro.js
+++ src/modules/fetchPro.js
@@ -1,19 +1,18 @@
 const fetchPro = async (url, uploadData = undefined) => {
-    const res = uploadData
-      ? await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(uploadData),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-      : await fetch(url);
-  
-    const data = await res.json();
-    const { error } = data;
-    if (!res.ok) throw new Error(`OPPs Something went wrong ${error.message} `);
-    return data;
-  };
-  
-  export default fetchPro;
-  
+  const res = uploadData
+    ? await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(uploadData),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+    : await fetch(url);
+
+  const data = await res.json();
+  const { error } = data;
+  if (!res.ok) throw new Error(`OPPs Something went wrong ${error.message} `);
+  return data;
+};
+
+export default fetchPro;
